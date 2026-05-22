a99e7b7213b67077251b540ac220d2bd325419e1
Madlangbayan <12403879@benilde.edu.ph>
2026-05-22 19:49:38 +0800

    Add favorite food variable configuration standard

--- profile.py
+++ profile.py
@@ -3,6 +3,8 @@ name = "Alex"           # String tracking alphabetic values
 age = 19                # Integer tracking discrete values
 is_student = True       # Boolean tracking state/conditions
 
+favorite_food = "Chocolate"     # Custom variable
+
 print("Name:", name)
 print("Age:", age)
 print("Student Status:", is_student)
